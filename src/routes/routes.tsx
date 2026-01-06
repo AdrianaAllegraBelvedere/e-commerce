f75d960ee459ef08438766df848a9b8275d12bab
Adriana Allegra <adriana.allegrabelvedere@gmail.com>
2026-01-06 20:27:06 -0300

    Add router configuration and route paths

---
+++ src/routes/routes.tsx
@@ -0,0 +1,39 @@
+import { createBrowserRouter } from "react-router-dom";
+import { CART, CHECKOUT, HOME, PRODUCT_DETAIL, SUCCESS } from "./paths";
+import App from "../App";
+import HomePage from "../pages/HomePage";
+import CartPage from "../pages/CartPage";
+import CheckoutPage from "../pages/CheckoutPage";
+import ProductPage from "../pages/ProductPage";
+import SuccessPage from "../pages/SuccessPage";
+
+
+
+export const router = createBrowserRouter([
+    {
+        path: HOME,
+        element: <App/>,
+        children:[
+            {
+                index: true,
+                element: <HomePage/>
+            },
+            {
+                path: CART,
+                element: <CartPage/>
+            },
+            {
+                path:CHECKOUT,
+                element:<CheckoutPage/>
+            },
+            {
+                path: PRODUCT_DETAIL,
+                element: <ProductPage/>
+            },
+            {
+                path: SUCCESS,
+                element: <SuccessPage/>
+            }
+        ]
+    }
+])
