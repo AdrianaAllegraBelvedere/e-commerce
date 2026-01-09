0506da247e95bdfb97127de94a9395213814998f
Adriana Allegra <adriana.allegrabelvedere@gmail.com>
2026-01-09 16:33:25 -0300

    add responsive header layout

--- src/components/HeaderComponent.tsx
+++ src/components/HeaderComponent.tsx
@@ -1,19 +1,43 @@
 import {  NavLink } from 'react-router-dom'
 import { HOME, CART } from '../routes/paths.ts'
+import logo from '../assets/logo.png'
+import '../styles/Header.css'
+
 
 const HeaderComponent = () => {
+
   return (
-      <header>
-        <nav>
-          <NavLink to={HOME}>
-            E-commerce MVP
+    <header className="appHeader">
+      
+      <div className="header">
+        <NavLink to={HOME} >
+          <img className="logo" src={logo} alt="Logo de Market" />
+        </NavLink>
+
+        <div className="searchContainer">
+          {/* icon */}
+          <svg xmlns="http://www.w3.org/2000/svg" height="20" viewBox="0 -960 960 960" width="20" className="searchIcon">
+            <path d="M784-120 532-372q-30 24-69 38t-83 14q-109 0-184.5-75.5T120-580q0-109 75.5-184.5T380-840q109 0 184.5 75.5T640-580q0 44-14 83t-38 69l252 252-56 56ZM380-400q75 0 127.5-52.5T560-580q0-75-52.5-127.5T380-760q-75 0-127.5 52.5T200-580q0 75 52.5 127.5T380-400Z"/>
+          </svg>
+
+          <input
+            className="searchInput"
+            type="search"
+            aria-label="Buscar productos"
+            placeholder="Buscar..."
+          />
+        </div>
+        
+          <NavLink to={CART} className="cartButton" aria-label="Ir al carrito">
+            <svg xmlns="http://www.w3.org/2000/svg" height="22" viewBox="0 -960 960 960" width="22">
+              <path d="M280-80q-33 0-56.5-23.5T200-160q0-33 23.5-56.5T280-240q33 0 56.5 23.5T360-160q0 33-23.5 56.5T280-80Zm400 0q-33 0-56.5-23.5T600-160q0-33 23.5-56.5T680-240q33 0 56.5 23.5T760-160q0 33-23.5 56.5T680-80ZM246-720l96 200h280l110-200H246Zm-38-80h590q23 0 35 20.5t1 41.5L692-482q-11 20-29.5 31T622-440H324l-44 80h480v80H280q-45 0-68-39.5t-2-78.5l54-98-144-304H40v-80h130l38 80Zm134 280h280-280Z"/>
+            </svg>
           </NavLink>
 
-          <NavLink to={CART}>
-            Cart
-          </NavLink>
-        </nav>
-      </header>
+      </div>
+
+
+    </header>
   )
 }
 
